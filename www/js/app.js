b8aa2148a1fc6e73243ca065aa16adfb0cd80dd8
mitv <mitv@bymo.ch>
2015-01-05 11:29:01 +0800

    first commit using firebase

--- www/js/app.js
+++ www/js/app.js
@@ -4,68 +4,196 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('cleverbaby', ['ionic', 'firebase', 'cleverbaby.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+.run(function ($ionicPlatform, $rootScope, $firebaseAuth, $firebase, $window, $ionicLoading, $timeout, $ionicModal) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+
+
+        $rootScope.userEmail = null;
+        $rootScope.baseUrl = 'https://cleverbaby.firebaseio.com/';
+        var authRef = new Firebase($rootScope.baseUrl);
+        $rootScope.auth = $firebaseAuth(authRef);
+
+        $rootScope.show = function (text) {
+            $rootScope.loading = $ionicLoading.show({
+                template: text ? text : 'Loading..',
+                showBackdrop: true,
+                delay: 0
+            });
+        };
+
+        $rootScope.hide = function () {
+            $ionicLoading.hide();
+        };
+
+        $rootScope.notify = function (text) {
+            $rootScope.show(text);
+            $window.setTimeout(function () {
+                $rootScope.hide();
+            }, 1999);
+        };
+
+        $rootScope.logout = function () {
+            $rootScope.auth.$logout();
+            $rootScope.checkSession();
+        };
+
+        $rootScope.checkSession = function () {
+            var auth = $rootScope.auth.$getAuth();
+
+
+            if (auth == null) {
+                // no user session exists
+                // $rootScope.userEmail = null;
+                $window.location.href = '#/auth/signin';
+                return;
+            }
+
+            if (auth.uid) {
+                // user authenticated with Firebase
+                $rootScope.userEmail = auth.uid;
+                console.log("firebase url", $rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail));
+                var bucketListRef = $firebase(new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail)));
+                $rootScope.fbData = bucketListRef.$asArray();
+                $window.location.href = ('#/app/diary');
+            } else {
+                // user is logged out
+                $rootScope.userEmail = null;
+                $window.location.href = '#/auth/signin';
+            }
+
+        }
+        $rootScope.checkSession();
+
+        $ionicModal.fromTemplateUrl('templates/newChoose.html', {
+            scope: $rootScope,
+            animation: 'slide-in-up'
+        }).then(function (modal) {
+            $rootScope.newmodal = modal;
+        });
+
+        $rootScope.openNewModal = function () {
+            $rootScope.newmodal.show();
+        };
+        $rootScope.closeNewModal = function () {
+            $rootScope.newmodal.hide();
+        };
+
+        //Cleanup the modal when we're done with it!
+        $rootScope.$on('$destroy', function () {
+            $rootScope.newmodal.remove();
+        });
+        // Execute action on hide modal
+        $rootScope.$on('modal.hidden', function (modal) {
+            // Execute action
+        });
+        // Execute action on remove modal
+        $rootScope.$on('modal.removed', function (modal) {
+            // Execute action
+        });
+
+
+        $rootScope.addNurse = function(breast, length, time) {
+            $rootScope.newmodal.hide();
+            var data = {
+                type: 'nurse',
+                breast: breast == undefined ? 'both' : breast,
+                length: length == undefined ? 0 : length,
+                time: time == undefined ? Date.now() : time,
+                created: Date.now(),
+                updated: Date.now()
+            }
+            $rootScope.fbData.$add(data);
+        }
+
+
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-  .state('app', {
-    url: "/app",
-    abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: "/search",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
-      }
-    }
-  })
-
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
+    .state('app', {
+        url: "/app",
+        abstract: true,
+        templateUrl: "templates/menu.html",
+        controller: 'AppCtrl'
     })
 
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+    .state('app.search', {
+        url: "/search",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/search.html"
+            }
+        }
+    })
+
+    .state('app.browse', {
+        url: "/browse",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/browse.html"
+            }
+        }
+    })
+        .state('app.diary', {
+            url: "/diary",
+            views: {
+                'menuContent': {
+                    templateUrl: "templates/diary.html",
+                    controller: 'DiaryCtrl'
+                }
+            }
+        })
+
+    .state('app.single', {
+        url: "/playlists/:playlistId",
+        views: {
+            'menuContent': {
+                templateUrl: "templates/playlist.html",
+                controller: 'PlaylistCtrl'
+            }
+        }
+    })
+
+
+    .state('auth', {
+        url: "/auth",
+        abstract: true,
+        templateUrl: "templates/auth.html"
+    })
+        .state('auth.signin', {
+            url: '/signin',
+            views: {
+                'auth-signin': {
+                    templateUrl: 'templates/auth-signin.html',
+                    controller: 'SignInCtrl'
+                }
+            }
+        })
+        .state('auth.signup', {
+            url: '/signup',
+            views: {
+                'auth-signup': {
+                    templateUrl: 'templates/auth-signup.html',
+                    controller: 'SignUpCtrl'
+                }
+            }
+        })
+
+
+    ;
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/diary');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,48 +1,275 @@
-angular.module('starter.controllers', [])
+angular.module('cleverbaby.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+.controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+        $scope.modal.hide();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Open the login modal
+    $scope.login = function () {
+        $scope.modal.show();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+        console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function () {
+            $scope.closeLogin();
+        }, 1000);
+    };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+.controller('DiaryCtrl', function ($scope, $rootScope, $ionicModal, $firebase) {
+
+
+
+    $scope.noData = true;
+
+    $scope.list = $rootScope.fbData;
+
+
+
+
+    /*
+    bucketListRef.on('value', function (snapshot) {
+        var data = snapshot.val();
+        console.log('data',snapshot);
+        $scope.list = [];
+        $scope.list = data;
+        if ($scope.list) {
+            if ($scope.list.length == 0) {
+                $scope.noData = true;
+            } else {
+                $scope.noData = false;
+            }
+        } else {
+
+            $scope.noData = true;
+        }
+
+
+    }); */
+
+    $ionicModal.fromTemplateUrl('templates/newItem.html', function (modal) {
+        $scope.newTemplate = modal;
+    });
+
+
+
+
+    $scope.newTask = function () {
+        $scope.newTemplate.show();
+    };
+
+    $scope.deleteItem = function (key) {
+        $rootScope.show("Please wait... Deleting from List");
+        var itemRef = new Firebase($rootScope.baseUrl + escapeEmailAddress($rootScope.userEmail));
+        bucketListRef.child(key).remove(function (error) {
+            if (error) {
+                $rootScope.hide();
+                $rootScope.notify('Oops! something went wrong. Try again later');
+            } else {
+                $rootScope.hide();
+                $rootScope.notify('Successfully deleted');
+            }
+        });
+    };
+
+    $scope.playlists = [
+        {
+            title: 'Reggae',
+            id: 1
+        },
+        {
+            title: 'Chill',
+            id: 2
+        },
+        {
+            title: 'Dubstep',
+            id: 3
+        },
+        {
+            title: 'Indie',
+            id: 4
+        },
+        {
+            title: 'Rap',
+            id: 5
+        },
+        {
+            title: 'Cowbell',
+            id: 6
+        }
   ];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('PlaylistCtrl', function ($scope, $stateParams) {})
+
+
+.controller('SignUpCtrl', [
+    '$scope', '$rootScope', '$firebaseAuth', '$window',
+    function ($scope, $rootScope, $firebaseAuth, $window) {
+        $scope.user = {
+            email: "",
+            password: ""
+        };
+        $scope.createUser = function () {
+            var email = this.user.email;
+            var password = this.user.password;
+
+            if (!email || !password) {
+                $rootScope.notify("Please enter valid credentials");
+                return false;
+            }
+
+            $rootScope.show('Please wait.. Registering');
+            return $rootScope.auth.$createUser(email, password)
+                .then(function () {
+                    // createUser success
+                    console.log("createUser success");
+
+                    $rootScope.hide();
+
+                    return $rootScope.auth.$authWithPassword({
+                        "email": email,
+                        "password": password
+                    }).then(function (authData) {
+                        console.log("Logged in as:", authData.uid);
+                        $rootScope.userEmail = authData.uid;
+                        $rootScope.checkSession();
+                        $window.location.href = ('#/app/diary');
+                    }).catch(function (error) {
+                        console.error("Authentication failed:", error);
+                    });
+
+
+
+
+
+
+
+                })
+
+            .catch(function (error) {
+                // createUser failed
+                console.error("Error: ", error);
+                $rootScope.hide();
+                if (error.code == 'INVALID_EMAIL') {
+                    $rootScope.notify('Invalid Email Address');
+                } else if (error.code == 'EMAIL_TAKEN') {
+                    $rootScope.notify('Email Address already taken');
+                } else {
+                    $rootScope.notify('Oops something went wrong. Please try again later');
+                }
+            });
+
+
+        }
+    }
+  ])
+
+.controller('SignInCtrl', [
+  '$scope', '$rootScope', '$firebaseAuth', '$window',
+  function ($scope, $rootScope, $firebaseAuth, $window) {
+        // check session
+        $rootScope.checkSession();
+        $scope.user = {
+            email: "",
+            password: ""
+        };
+        $scope.validateUser = function () {
+            $rootScope.show('Please wait.. Authenticating');
+            var email = this.user.email;
+            var password = this.user.password;
+            if (!email || !password) {
+                $rootScope.notify("Please enter valid credentials");
+                return false;
+            }
+
+            $rootScope.auth.$authWithPassword({
+                email: email,
+                password: password
+            })
+                .then(function (user) {
+                    $rootScope.hide();
+                    $rootScope.userEmail = user.email;
+                    $rootScope.checkSession();
+                    $window.location.href = ('#/app/diary');
+                }, function (error) {
+                    $rootScope.hide();
+                    if (error.code == 'INVALID_EMAIL') {
+                        $rootScope.notify('Invalid Email Address');
+                    } else if (error.code == 'INVALID_PASSWORD') {
+                        $rootScope.notify('Invalid Password');
+                    } else if (error.code == 'INVALID_USER') {
+                        $rootScope.notify('Invalid User');
+                    } else {
+                        $rootScope.notify('Oops something went wrong. Please try again later');
+                    }
+                });
+        }
+  }
+])
+
+
+.controller('newCtrl', function ($rootScope, $scope, $window, $firebase) {
+    $scope.data = {
+        item: ""
+    };
+
+    $scope.close = function () {
+        $scope.modal.hide();
+    };
+
+    $scope.createNew = function () {
+        var item = this.data.item;
+
+        if (!item) return;
+
+        console.log("itme", item);
+
+        $scope.modal.hide();
+
+        $rootScope.show("Please wait... Creating new");
+
+        var form = {
+            item: item,
+            isCompleted: false,
+            created: Date.now(),
+            updated: Date.now()
+        };
+
+        $rootScope.fbData.$add(form);
+
+
+        $rootScope.hide();
+    };
+})
+
+
+;
+
+
+
+
+function escapeEmailAddress(email) {
+    if (!email) return false
+        // Replace '.' (not allowed in a Firebase key) with ','
+    email = email.toLowerCase();
+    email = email.replace(/\./g, ',');
+    return email.trim();
+};
