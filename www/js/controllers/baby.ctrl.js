0c6216c679f8682400a1b7db485b5d85e271c028
Nyoman Dharmawan <blaukngindang@gmail.com>
2015-04-18 07:44:11 +0800

    add baby save and cancel

--- www/js/controllers/baby.ctrl.js
+++ www/js/controllers/baby.ctrl.js
@@ -1,6 +1,19 @@
 angular.module('cleverbaby.controllers')
 .controller('BabyCtrl', ['$scope', function ($scope) {
+	
+
 	$scope.cancel = function(){
 		$scope.modal.hide();
+	}
+	$scope.saveBaby = function(){
+		var birtday = this.addbaby.birtday;
+		var name = this.addbaby.name;
+		var gender = this.addbaby.gender;
+		var baby = {
+			birtday : birtday,
+			name : name,
+			gender: gender
+		};
+		console.log(baby);
 	}
 }])
