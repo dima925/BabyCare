f66800584f731b81994032d09632a5ae030155ea
promentol <promentol@gmail.com>
2015-05-08 18:05:21 +0400

    activities add, and timeline

--- www/js/helpers/showActivity.js
+++ www/js/helpers/showActivity.js
@@ -1,27 +1,6 @@
 angular
     .module('cleverbaby.helpers')
     .filter('showActivity', function(){
-        var table = {
-            'bottle': 'bottle',
-            'pump': 'pumping',
-            'solid': 'solid',
-            'change': 'diapers',
-            'sleep': 'sleep',
-            'milestone': 'milestone',
-            'growth': 'growth',
-            'bath': 'bath',
-            'play': 'play',
-            'doctor': 'doctor',
-            'sick': 'sickness',
-            'temperature': 'temp',
-            'medication': 'medication',
-            'vaccination': 'vaccination',
-            'allergy': 'alergy',
-            'mood': 'mood',
-            'moment': 'activity',
-            'diary': 'diary',
-            'todo': 'todo'
-        };
         return function(activity){
             var str = "";
 
@@ -41,6 +20,125 @@ angular
 
             if(activity.type == 'change'){
                 str += (activity.diaper_type || "");
+            }
+
+            if(activity.type == 'solid'){
+                if(activity.food_type){
+                    str += "Ate " + activity.food_type;
+                }
+            }
+
+            if(activity.type == 'pump'){
+                if(activity.side){
+                    str += activity.side + " ";
+                }
+                if(activity.amount){
+                    str += activity.amount+"oz";
+                }
+            }
+
+            if(activity.type == 'todo'){
+                if(activity.notes){
+                    str += activity.notes;
+                }
+            }
+
+            if(activity.type == 'mood'){
+                if(activity.mood_type){
+                    str += activity.mood_type;
+                }
+            }
+
+            if(activity.type == 'play'){
+                if(activity.notes){
+                    str += activity.notes;
+                }
+            }
+
+            if(activity.type == 'bath'){
+                if(activity.notes){
+                    str += activity.notes;
+                } else{
+                    str += "Had a bath";
+                }
+            }
+
+            if(activity.type == 'medication'){
+                if(activity.drug){
+                    str += "Took ";
+                    if(activity.amount_given){
+                        str+=activity.amount_given+" ";
+                    }
+                    str += activity.drug;
+                }
+            }
+
+            if(activity.type == 'diary'){
+                if(activity.notes){
+                    str += activity.notes;
+                }
+            }
+
+            if(activity.type == 'milestone'){
+                if(activity.milestone_type){
+                    str += activity.milestone_type;
+                }
+            }
+
+            if(activity.type == 'sick'){
+                if(activity.symptom){
+                    str += activity.symptom;
+                }
+            }
+
+            if(activity.type == 'doctor'){
+                if(activity.visit_type){
+                    str += activity.visit_type;
+                }
+            }
+
+            if(activity.type == 'vaccination'){
+                if(activity.vaccination_type){
+                    str += activity.vaccination_type;
+                }
+            }
+
+            if(activity.type == 'growth'){
+                if(activity.weight){
+                    str += activity.weight;
+                }
+                if(activity.height){
+                    str += activity.height;
+                }
+            }
+
+            if(activity.type == 'sleep'){
+                if(activity.time_slept){
+                    str+="Slept for "+activity.time_slept;
+                }
+            }
+
+            if(activity.type == 'temperature'){
+                if(activity.temp){
+                    str+=activity.temp+"c";
+                }
+            }
+
+            if(activity.type == 'allergy'){
+                if(activity.source){
+                    if( activity.severity ){
+                        str += activity.severity + " reaction to ";
+                    } else{
+                        str += "Reaction to";
+                    }
+                    str += activity.source;
+                }
+            }
+
+            if(activity.type == 'moment'){
+                if(activity.notes){
+                    str += activity.notes;
+                }
             }
             return str;
         }
