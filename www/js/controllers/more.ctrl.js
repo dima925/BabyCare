83b8c15ffa28ef2d4ef0d6caf85266ed845f3c4f
Kevin Montesclaros <kevinmarkmontesclaros@yahoo.com>
2015-05-16 10:44:22 +0800

    task: fixed the merge conflict in more stuffs.(your account page)

--- www/js/controllers/more.ctrl.js
+++ www/js/controllers/more.ctrl.js
@@ -1,34 +1,6 @@
 angular.module('cleverbaby.controllers')
     .controller('MoreCtrl', ['$scope', '$timeout', '$state', '$cordovaSocialSharing', '$cordovaFacebook', '$ionicModal',
         function($scope, $timeout, $state, $cordovaSocialSharing, $cordovaFacebook, $ionicModal) {
-            //todo temporary data
-            $scope.data = {name: "Roger"};
-
-            var originalData = angular.toJson($scope.data, true);
-
-            $scope.inputDisabled = true;
-            /**
-             * enables the editing of form
-             */
-            $scope.activateEdit = function() {
-                $scope.inputDisabled = false;
-                originalData = angular.toJson($scope.data, true);
-            };
-
-            $scope.cancelEdit = function() {
-                $scope.inputDisabled = true;
-                $scope.data = angular.fromJson(originalData);
-            };
-
-            $scope.saveEdit = function() {
-                $scope.inputDisabled = true;
-            };
-
-
-            $scope.$on('$stateChangeStart',
-                function(event, toState, toParams, fromState, fromParams){
-                    $scope.inputDisabled = true;
-                })
 
             $scope.go = function(state, params) {
                 $state.go(state, params);
