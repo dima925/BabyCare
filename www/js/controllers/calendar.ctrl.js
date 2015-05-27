bf5db614aa9179db33064e1f98ea9f3bebeb8488
ShaderZAK <m.sevak@gmail.com>
2015-05-27 10:23:27 +0400

    Show real data in the Calendar Tab (Part 1)

--- www/js/controllers/calendar.ctrl.js
+++ www/js/controllers/calendar.ctrl.js
@@ -3,22 +3,89 @@
  */
 
 angular.module('cleverbaby.controllers')
-    .controller('CalendarCtrl', ['$scope',
-        function ($scope) {
+    .controller('CalendarCtrl', ['$scope', '$rootScope', 'ActivityService',
+        function($scope, $rootScope, ActivityService) {
+
+            $scope.hasContent = false;
+            $scope.selectedDay = '';
+            $scope.selectedMonth = '';
+            $scope.selectedYear = '';
+
+            $scope.activities = [];
+            
             $scope.eventSources = [];
+
+            var date = new Date();
+            var d = date.getDate();
+            var m = date.getMonth();
+            var y = date.getFullYear();
+
+            start = 0;
+            limit = 10;
+            ActivityService.getAllActivitiesByBabyId($rootScope.baby.uuid, start, limit).then(function(activities){
+                $scope.activities = activities;
+            });
+
+            /* event sources array*/
+            /*$scope.eventSources = [{
+                events: [{
+                        title: '',
+                        start: '2015-05-04',
+                        allDay: true,
+                        backgroundColor: 'transparent',
+                    }, {
+                        title: '',
+                        start: '2015-05-13',
+                        allDay: true,
+                        backgroundColor: 'transparent',
+                    }
+                    // etc...
+                ],
+                //color: 'yellow', // an option!
+                //textColor: 'yellow' // an option!
+            }];*/
+
+            $scope.alertOnEventClick = function() {
+            };
+
+            $scope.alertOnDayClick = function(date) {
+                // when user clicks on day
+                if(date.getMonth() == m && date.getDate() == d) {
+                    $scope.hasContent = true;
+                    $scope.selectedDay = d;
+                    $scope.selectedMonth = m;
+                    $scope.selectedYear = y;
+
+                } else {
+                    $scope.hasContent = false;
+                }
+            };
+
+            $scope.eventRender = function(event, element, view) {
+            };
 
             /* config object */
             $scope.uiConfig = {
-                calendar:{
+                calendar: {
                     editable: true,
-                    header:{
+                    header: {
                         left: 'prev',
                         center: 'title',
                         right: 'next'
                     },
-                    dayClick: $scope.alertEventOnClick,
+                    dayClick: $scope.alertOnDayClick,
+                    eventClick: $scope.alertOnEventClick,
                     eventDrop: $scope.alertOnDrop,
-                    eventResize: $scope.alertOnResize
+                    eventResize: $scope.alertOnResize,
+                    eventRender: $scope.eventRender,
+                    dayRender: function(date, cell) {
+                        if(date.getMonth() == m && date.getDate() == d) {
+                            $(cell).addClass('calendar-highlight-circle');
+                        }
+                    }
                 }
             };
-        }]);
+
+
+        }
+    ]);
