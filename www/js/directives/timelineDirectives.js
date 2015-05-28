832de5a28af3aa03dc8102e5b697f6786e27364d
Kevin Montesclaros <kevinmarkmontesclaros@yahoo.com>
2015-05-28 15:22:53 +0800

    task: change the logic for the filling activities in alternative timeline page. Use css 'left' to move the activities to the correct positive. As well css changes.

--- www/js/directives/timelineDirectives.js
+++ www/js/directives/timelineDirectives.js
@@ -5,7 +5,7 @@ angular.module('cleverbaby.directives')
             templateUrl: 'templates/elements/timeline-chart.html',
             link: function (scope, element) {
 
-                ActivityService.getAllActivitiesByBabyId($rootScope.babyId, 0, 1000).then(function(activities){
+                ActivityService.getAllActivitiesByBabyId($rootScope.babyId, 0, 100).then(function(activities){
 
                     var dateToday = moment(new Date()).format("MM-DD-YYYY");
                     var dateYesterday = moment(new Date()).subtract(1, 'days').format("MM-DD-YYYY");
@@ -30,6 +30,7 @@ angular.module('cleverbaby.directives')
                     angular.forEach(activities, function(value, index){
                         var valueDateStart = moment(value.time).format("MM-DD-YYYY");
                         var valueDateEnd = moment(value.time_end).format("MM-DD-YYYY");
+                        
                         if(unorderedDate[valueDateStart]){
                             unorderedDate[valueDateStart].activities.unshift(value);
                         }else{
@@ -47,14 +48,49 @@ angular.module('cleverbaby.directives')
                     //(fillpx / 332) x 100
                     var timelineIonContentWidth = parseInt(screen.width); //332px sample
 
+                    //parseInt(angular.element('.ul.timeline-grid li').css('width').substring(0, this.length - 1));
+                    /****** IMPORTANT ul.timeline-grid, the right width for this is required currently its 144.8% ******/
+
+                    var widthPerBlockPercentage = 144.8 / 100;
+                    var widthOfBlockCon = widthPerBlockPercentage * timelineIonContentWidth;
+
+
+
                     function calculateDurationPercentage(startTime, endTime){
                         var startTime = moment.duration(startTime);
                         var valueDateEnd = moment.duration(endTime);
                         var durationMinute = valueDateEnd.subtract(startTime).asMinutes(); //
-                        var durationPercentage = (timelineIonContentWidth/1440) * durationMinute;
-                        var fillPercentage = (durationPercentage/ timelineIonContentWidth) * 100;
+                        var durationPercentage = (widthOfBlockCon/1440) * durationMinute;
+                        var fillPercentage = (durationPercentage/ widthOfBlockCon) * 100;
 
                         return fillPercentage;
+                    }
+
+                    /**
+                     * calculates
+                     * @param activityStartTime - date of activity
+                     * @returns (int) the margin left percentage
+                     */
+                    function calculateMarginLeftPercentage(activityStartTime) {
+                        //8 blocks in one grid
+                        //widthOfBlockCon / 8
+                        //3hours per block
+                        /*
+                            eg. starttime is 9am
+                            9am - 6am = 3hrs
+                            //one block is 3 hrs gap so divide by 3
+                            (3 / 3) * lengthPerBlank
+                         */
+
+                        var startTime = moment(activityStartTime).set({'hour': 6, 'second': 00, 'minute': 00});
+                        var lengthPerBlank = widthOfBlockCon / 8;
+
+                        startTime = moment.duration(startTime);
+                        var valueDateEnd = moment.duration(activityStartTime);
+                        var durationHours = valueDateEnd.subtract(startTime).asHours();
+
+                        var marginLeftPercentage = (durationHours / 3) * lengthPerBlank;
+                        return marginLeftPercentage;
                     }
 
                     function getFillPercentages(activity){
@@ -65,15 +101,17 @@ angular.module('cleverbaby.directives')
                         angular.forEach(activity, function(activity, index){
                             var timeEnd;
                             if(activity.type == "sleep"){
-                                timeEnd = activity.sleep_timeend;
+                                timeEnd = moment(activity.sleep_timeend);
                             }else{
-                                timeEnd = angular.isDefined(activity.time_end) ? activity.time_end : moment(activity.time).add(10, 'm');
+                                timeEnd = angular.isDefined(activity.time_end) ? moment(activity.time_end) : moment(activity.time).add(10, 'm');
 
                             }
-                            fillAray.push({'percentage': calculateDurationPercentage(activity.time, timeEnd), 'type': activity.type, 'startTime': activity.time, 'endTime': timeEnd});
+                            fillAray.push({'marginLeft': calculateMarginLeftPercentage(activity.time), 'percentage': calculateDurationPercentage(activity.time, timeEnd), 'type': activity.type, 'startTime': activity.time, 'endTime': timeEnd});
                         });
 
                         //this part is to get the blank percentage on the timeline
+
+                        /*
                         angular.forEach(fillAray, function(activity, index) {
                             var activityDate = moment(activity.startTime);
 
@@ -95,16 +133,21 @@ angular.module('cleverbaby.directives')
 
                             finalFillAray.push(activity);
                         });
-                        return finalFillAray;
+                        */
+                        return fillAray;
                     }
 
-                    //create the fill percentage
-                    angular.forEach(unorderedDate, function(dateArrayActivity, index){
-                        dateArrayActivity.fill = getFillPercentages(dateArrayActivity.activities);
-                    });
+                    scope.orderedDateFinal = [];
 
-                    scope.orderedDateFinal = unorderedDate;
+                    //create the fill percentage && rearrange latest to oldest
+                    angular.forEach(unorderedDate, function(dateArrayActivity, index){
+                        dateArrayActivity.fill = getFillPercentages(dateArrayActivity.activities)
+                        scope.orderedDateFinal.push(dateArrayActivity);
+                    });
+                    scope.orderedDateFinal.reverse();
+
                     console.log(scope.orderedDateFinal);
+
                 });
             }
         }
