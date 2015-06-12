33b7d63209812169b5ab22e2e7a9afc4f5cd9566
ShaderZAK <m.sevak@gmail.com>
2015-06-12 09:37:41 +0400

    ETA Settings LocalStorage Reset state bug fix

--- www/js/controllers/eta.settings.ctrl.js
+++ www/js/controllers/eta.settings.ctrl.js
@@ -17,45 +17,46 @@ angular.module('cleverbaby.controllers')
 	            if($scope.etaSettingsModal.mode == 'feed') {
 	            	var feedMins = ActivityService.getActivityEtaByType($rootScope.babyId, 'feed');
 
-	            	$scope.modalEtaTime = moment.duration(feedMins, 'minutes').humanize();
+	            	if(feedMins)
+	            		$scope.modalEtaTime = moment.duration(feedMins, 'minutes').humanize();
+	            	else
+	            		$scope.modalEtaTime = 'Not available';
 
-	            	if($localStorage.settings && $localStorage.settings.eta && $localStorage.settings.eta.feed && !isNaN($localStorage.settings.eta.feed) && $localStorage.settings.eta.feed > 0) {
+	            	if($localStorage.settings && $localStorage.settings.eta && $localStorage.settings.eta.feed && !isNaN($localStorage.settings.eta.feed)) {
 	            		$scope.etaSettingsModal.eta_fixed_duration = $localStorage.settings.eta.feed;
 	            	}
 	            } else {
 	            	var diaperMins = ActivityService.getActivityEtaByType($rootScope.babyId, 'diaper');
 	            	
-	            	$scope.modalEtaTime = moment.duration(diaperMins, 'minutes').humanize();
+	            	if(diaperMins)
+	            		$scope.modalEtaTime = moment.duration(diaperMins, 'minutes').humanize();
+	            	else
+	            		$scope.modalEtaTime = 'Not available';
 
-	            	if($localStorage.settings && $localStorage.settings.eta && $localStorage.settings.eta.diaper && !isNaN($localStorage.settings.eta.diaper) && $localStorage.settings.eta.diaper > 0) {
+	            	if($localStorage.settings && $localStorage.settings.eta && $localStorage.settings.eta.diaper && !isNaN($localStorage.settings.eta.diaper)) {
 	            		$scope.etaSettingsModal.eta_fixed_duration = $localStorage.settings.eta.diaper;
 	            	}
 	            }
-
-	            unwatch = $scope.$watch('etaSettingsModal.eta_fixed_duration', function (value) {
-	            	if(typeof value == 'undefined')
-	            		value = -1;
-
-	            	if(!$localStorage.settings)
-	            		$localStorage.settings = {};
-
-	            	if(!$localStorage.settings.eta)
-	            		$localStorage.settings.eta = {};
-
-	            	if($scope.etaSettingsModal.mode == 'feed') {
-	            		$localStorage.settings.eta.feed = value;
-	            	} else {
-	            		$localStorage.settings.eta.diaper = value;
-	            	}
-	            });
 			});
 
 			// Execute action on hide modal
 			$scope.$on('modal.hidden', function() {
-				if(unwatch) {
-					unwatch();
-					unwatch = null;
-				}
+				var value = $scope.etaSettingsModal.eta_fixed_duration;
+
+				if(typeof value == 'undefined')
+            		value = -1;
+
+            	if(!$localStorage.settings)
+            		$localStorage.settings = {};
+
+            	if(!$localStorage.settings.eta)
+            		$localStorage.settings.eta = {};
+
+            	if($scope.etaSettingsModal.mode == 'feed') {
+            		$localStorage.settings.eta.feed = value;
+            	} else {
+            		$localStorage.settings.eta.diaper = value;
+            	}
 			});
         }
     ]);
