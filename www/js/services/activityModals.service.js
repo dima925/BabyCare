56f69820b375368e80a4005cdc5e93d9b25326e1
anatoly <>
2015-06-17 11:52:46 -0400

    Fixed to update value

--- www/js/services/activityModals.service.js
+++ www/js/services/activityModals.service.js
@@ -6,34 +6,34 @@ angular
             modals: {}
         };
 
-        var activityTypes = [
-            'nurse',
-            'bottle',
-            'pump',
-            'diaper',
-            'solid',
-            'sleep',
-            'milestone',
-            'growth',
-            'bath',
-            'play',
-            'doctor',
-            'sick',
-            'temperature',
-            'medication',
-            'vaccination',
-            'allergy',
-            'mood',
-            'moment',
-            'diary',
-            'todo'
-        ];
-
-        activityTypes.forEach(function(x){
-            $ionicModal.fromTemplateUrl('templates/activities/'+x+'.html',function(modal){
-                exports.modals[x] = modal;
-            });
-        });
+//        var activityTypes = [
+//            'nurse',
+//            'bottle',
+//            'pump',
+//            'diaper',
+//            'solid',
+//            'sleep',
+//            'milestone',
+//            'growth',
+//            'bath',
+//            'play',
+//            'doctor',
+//            'sick',
+//            'temperature',
+//            'medication',
+//            'vaccination',
+//            'allergy',
+//            'mood',
+//            'moment',
+//            'diary',
+//            'todo'
+//        ];
+//
+//        activityTypes.forEach(function(x){
+//            $ionicModal.fromTemplateUrl('templates/activities/'+x+'.html',function(modal){
+//                exports.modals[x] = modal;
+//            });
+//        });
 
         function initialData() {
             var data = {};
@@ -43,15 +43,19 @@ angular
         }
 
         exports.showModal = function(type, showData){
-
             var x = showData ? angular.copy(showData, x) : initialData(type);
-
-            function showModal(modal){
-                modal.mode = showData ? 'edit':'add';
-                modal.data = x;
-                modal.show();
-            }
-            exports.modals[type] && showModal(exports.modals[type]);
+            if(exports.modals[type])
+                exports.modals[type].remove();
+            exports.modals[type] = {};
+            $ionicModal.fromTemplateUrl('templates/activities/'+type+'.html', {}).then(function(modal){
+                exports.modals[type] = modal;
+                if (exports.modals[type]){                    
+                    exports.modals[type].mode = showData ? 'edit':'add';
+                    exports.modals[type].data = x;                    
+                    exports.modals[type].show();
+                    
+                }                
+            });            
         };
 
         return exports;
